1199cfc1a88aab414a4f0f89e3c33d5ada3eb817
Alan Tang <alan.tang.astro@gmail.com>
2025-01-14 11:35:29 -0500

    inital commit: basic options, keymaps, lazy. treesitter & colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+	-- "folke/tokyonight.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function() 
+		-- vim.cmd([[colorscheme tokyonight]])
+		vim.cmd([[colorscheme catppuccin-macchiato]])
+	end,
+}
