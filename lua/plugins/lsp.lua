40e8824f593ea5129618c969a328652dcd7eb291
Alan Tang <alan.tang.astro@gmail.com>
2025-06-08 21:38:04 -0400

    stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,13 +20,9 @@ return {
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'williamboman/mason.nvim',           opts = {} },
       { 'williamboman/mason-lspconfig.nvim', opts = {} },
-      -- 'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
       -- { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Allows extra capabilities provided by nvim-cmp
-      -- 'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
       local lspconfig = require('lspconfig')
