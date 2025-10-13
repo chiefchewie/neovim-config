ab3f38137e5fe5a85aa1aba3f97e630a76ef8543
Alan Tang <alan.tang.astro@gmail.com>
2025-10-13 01:56:31 -0400

    add blink.cmp

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -20,11 +20,8 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- automatically check for plugin updates
   checker = { enabled = true },
   rocks = { enabled = false },
 })
