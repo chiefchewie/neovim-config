b1dab8190d8e1175342a4f52f8e9cd557b7ffdd4
Alan Tang <alan.tang.astro@gmail.com>
2025-10-14 16:56:10 -0400

    lazy load blink

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,17 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  event = { "BufReadPost", "BufNewFile" },
+  event = 'VimEnter',
   config = function()
     local configs = require('nvim-treesitter.configs')
     configs.setup({
-      ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'html' },
+      ensure_installed = { 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'typescript', 'html', 'python', 'rust', 'zig' },
       sync_install = false,
       auto_install = false,
       ignore_install = {},
       modules = {},
       highlight = {
         enable = true,
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
         additional_vim_regex_highlighting = false,
       },
       indent = { enable = false },
