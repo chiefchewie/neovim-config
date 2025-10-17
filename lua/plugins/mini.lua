9224be4e3b3cb84e2a70a600f44c8acfeb197bd2
Alan Tang <alan.tang@shopify.com>
2025-10-17 15:01:11 -0400

    remove stuff that vscode-neovim won't use

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,17 +3,6 @@ return
   'nvim-mini/mini.nvim',
   version = false,
   config = function()
-    -- text editing
     require('mini.surround').setup()
-
-    -- general
-    require('mini.git').setup()
-    require('mini.diff').setup()
-
-    -- appearance
-    require('mini.icons').setup()
-    if not vim.g.vscode then
-      require('mini.statusline').setup()
-    end
   end
 }
