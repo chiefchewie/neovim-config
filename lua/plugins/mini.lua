e52892d4ded9b4c20f3c38edf51e0c9527f14454
Alan Tang <alan.tang.astro@gmail.com>
2025-11-20 22:36:00 -0500

    add mini.pick

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,11 +10,27 @@ return
     require('mini.surround').setup()
 
     -- general
-    require('mini.git').setup()
     require('mini.diff').setup()
+    require('mini.git').setup()
+    require('mini.pick').setup()
+    require('mini.extra').setup()
 
     -- appearance
     require('mini.icons').setup()
     require('mini.statusline').setup()
-  end
+  end,
+  keys = {
+    { "<leader>ff", function() MiniPick.builtin.files() end,                            desc = "Find Files" },
+    { "<leader>fb", function() MiniPick.builtin.buffers() end,                          desc = "Find Buffers" },
+    { "<leader>fg", function() MiniPick.builtin.grep_live() end,                        desc = "Live Grep" },
+    { "<leader>fG", function() MiniPick.builtin.grep({ pattern = '<cword>' }) end,      desc = "Find Grep" },
+    { "<leader>fd", function() MiniExtra.pickers.diagnostic({ scope = 'all' }) end,     desc = "Find Diagnostics (all)" },
+    { "<leader>fD", function() MiniExtra.pickers.diagnostic({ scope = 'current' }) end, desc = "Find Diagnostics (buf)" },
+    { "<leader>fm", function() MiniExtra.pickers.marks() end,                           desc = "Find Marks" },
+    { "<leader>fh", function() MiniPick.builtin.help() end,                             desc = "Find Help" },
+
+    { "<leader>/",  function() MiniExtra.pickers.history({ scope = '/' }) end,          desc = "'/' History" },
+    { "<leader>:",  function() MiniExtra.pickers.history({ scope = ':' }) end,          desc = "':' History" },
+    { "<leader>e",  function() MiniExtra.pickers.explorer() end,                        desc = "File Explorer" },
+  }
 }
